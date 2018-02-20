f38f70f7caed2ea26b2e685dbbb86b947c8c383d
Adam <adam.brown@digital.justice.gov.uk>
2018-02-20 14:05:03 +0000

    Added isset to cookie to stop notice error

--- components/view-like-button.php
+++ components/view-like-button.php
@@ -37,7 +37,7 @@ function like_button_run()
       }
 
     // Required to format the cookie's array correctly into the post ID.
-    if (is_string($_COOKIE['like-button-for-wordpress-plugin'])) {
+    if (is_string(isset($_COOKIE['like-button-for-wordpress-plugin']))) {
         $posts = unserialize($posts);
     }
 
@@ -75,7 +75,7 @@ function like_button_run()
       }
 
     // Required to format the cookie's array correctly into the post ID.
-    if (is_string($_COOKIE['like-button-for-wordpress-plugin-comments'])) {
+    if (is_string(isset($_COOKIE['like-button-for-wordpress-plugin-comments']))) {
         $comments = unserialize($comments);
     }
       // Check if the comment id is in the cookie. If it is then display else so visitor can't click again.
